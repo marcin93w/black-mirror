b7d8b6f980b0d83c57f37a999bc6aad52644166c
marcin93w <marcin93w@gmail.com>
2023-12-09 16:39:44 +0100

    Add relay switch

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import RPi.GPIO as GPIO
 
 def callback(channel):
     if GPIO.input(channel):
-        print("GPIO pin", channel, "is HIGH")
+        relay_on()
+        print("motion detected")
     else:
-        print("GPIO pin", channel, "is LOW")
+        relay_off()
+        print("motion not detected")
+
+def relay_on():
+    GPIO.setup(23, GPIO.OUT)
+
+def relay_off():
+    GPIO.setup(23, GPIO.IN)
 
 def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(23, GPIO.IN)
     GPIO.add_event_detect(18, GPIO.BOTH, callback=callback)
     print("motion listener registered")
 
